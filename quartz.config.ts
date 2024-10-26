b97d775d615611b8796d4a92520f927a53213ba9
Ian Wagner <ian.wagner@stadiamaps.com>
2024-10-26 14:11:39 +0900

    Quartz sync: Oct 26, 2024, 2:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "garden.ianwwagner.com",
+    baseUrl: "ianwwagner.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
